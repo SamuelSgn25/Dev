feafe5c76ddfcdff7159f2e4b53b121338f3f410
SamuelSgn25 <soglohounsamuel2@gmail.com>
2025-08-24 11:26:19 +0100

    retrieve one comment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc' // <-- Remplace `@vitejs/plugin-react` par `@vitejs/plugin-react-swc`
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   base: "/",
